a935b193f2983a99cfcd8d92ffe8783c466533ef
Avik Datta <avik26091998@gmail.com>
2017-12-31 15:53:43 +0530

    Update and rename Minimum_Range_Query.cpp to Range_Query.cpp

---
+++ Segment Tree/Range_Query.cpp
@@ -0,0 +1,72 @@
+//  build_tree: O(N lg(N))  
+//  update_tree: O(lg(N + k))
+//  query_tree: O(lg(N + k))
+// 
+//  k - No. of segments
+
+#include<bits/stdc++.h>
+#define ll long long
+#define inf 0x7fffffff
+using namespace std;
+
+int arr[10001], tree[4*10001];
+
+void build_tree(int node, int a, int b){
+
+    if(a > b) return;
+    if(a == b){
+        
+        tree[node] = arr[a];
+        return;
+    }
+
+    build_tree(2 * node, a, (a + b) / 2);
+    build_tree(2 * node + 1, 1 +((a + b) / 2) , b);
+
+    tree[node] = max(tree[node * 2], tree[node * 2 + 1]);
+
+}
+
+void update_tree(int node, int a, int b, int i, int j, int value){
+
+    if(a > b || a > j || b < i) return;
+
+    if(a == b) {tree[node] += value; return;}
+
+    update_tree(2 * node, a, (a + b) / 2, i, j, value);
+    update_tree(2 * node + 1, (a + b) / 2 + 1, b, i, j, value);
+
+    tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+ }
+
+ int query_tree(int node, int a, int b, int i, int j){
+
+    if(a > b || a > j || b < i) return -inf;
+    if(a >= i && b <= j) return tree[node];
+
+    int q1 = query_tree(2 * node, a, (a + b) / 2, i, j);
+    int q2 = query_tree(2 * node + 1, 1 + (a + b) / 2 , b, i, j);
+    
+    return max(q1, q2);
+    
+ }
+
+int main(){
+
+    int n;
+    
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+
+    build_tree(1, 0, n - 1);
+    
+    //update_tree(1, 0, n - 1, 1, 4, 3);
+    //update_tree(1, 0, n - 1, 2, 3, 1);
+
+    cout << query_tree(1, 0, n - 1, 1, 4);
+
+
+    return 0;
+
+}
