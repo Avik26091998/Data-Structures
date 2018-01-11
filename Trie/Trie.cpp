634da4d0bfcba2f48b5c5197fde8d9532d46de73
Avik Datta <avik26091998@gmail.com>
2018-01-11 11:37:51 +0530

    Update Trie.cpp

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -1,102 +1,107 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define MAX 1000000000
-     
+// The complexity of creating a trie is O(W*L), 
+// where W is the number of words, and L is an average length of the word: 
+// you need to perform L lookups on the average for each of the W words in the set.
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
+struct node{
 
-struct Node{
-
-  int isWord;
-  Node *child[26];
+  int isword;
+  node *child[26];
 
 };
 
-Node* NewNode(){
+node *Newnode(){
 
-  Node *new_node = new Node;
-  new_node->isWord = 0;
+  node* new_node = new node;
+  new_node->isword = 0;
   for(int i = 0; i < 26; ++i) new_node->child[i] = NULL;
-
   return new_node;
+
 }
 
-Node* addword(Node *root, string s){
+node* addword(node *root, string s){
 
-  Node *temp = root;
+  node *temp = root;
   root = temp;
+
+  for(int i = 0; i < s.length(); ++i){
+
+      int ch = s[i] - 'a';
+      if(temp->child[ch] == NULL) temp->child[ch] = Newnode();
+      temp = temp->child[ch];
+  }
+
+  temp->isword = 1;
+
+  return root;
+
+}
+
+int findword(node *root, string s){
+
+  node *temp = root;
+
   for(int i = 0; i < s.length(); ++i){
 
     int ch = s[i] - 'a';
-
-    if(temp->child[ch] == NULL) temp->child[ch] = NewNode();
-
-    temp = temp->child[ch];
-  }
-  temp->isWord = 1;
-
-  return root;
-}
-
-int FindWord(Node *root, string s){
-
-  Node *temp = root;
-
-  int ch;
-
-  for(int i = 0; i < s.length(); ++i){
-
-    ch = s[i] - 'a';
-
     if(temp->child[ch] == NULL) return 0;
-    temp = temp->child[ch];
+    else temp = temp->child[ch];
   }
-  if(temp->isWord == 1) return 1;
-  
-  return 0;
+
+  if(temp->isword == 1) return 1;
+  else return 0;
 }
 
-int isEmpty(Node *root){
+int isempty(node *root){
 
   for(int i = 0; i < 26; ++i){
 
-    if(root->child[i] != NULL) return 1;
+    if(root->child[i] != NULL) return 0;
   }
-  return 0;
-
+  return 1;
 }
 
-void Remove(string s, Node *temp,int index, int n){
+void remove(node **root, string s, int index, int n){
 
-  if(temp == NULL) return;
+  if(*root == NULL) return;
   if(index == n){
 
-    temp->isWord = 0;
-    if(isEmpty(temp)) delete[] temp;
+    if((*root)->isword == 1) (*root)->isword = 0;
+    if(isempty(*root)) delete[] *root;
     return;
   }
-  Remove(s, temp->child[s[index] - 'a'], index + 1, n);
+
+  remove(&(*root)->child[s[index] - 'a'], s, index + 1, n);
+  if(isempty(*root)) delete[] *root;
+
 }
 
 
 int main(){
 
- // ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-
-  Node *root = NewNode();
+  node *root = Newnode();
   root = addword(root, "avik");
+  root = addword(root, "ava");
   root = addword(root, "pogba");
-  root = addword(root, "manchester");
-  root = addword(root, "manu");
-  
-  cout << FindWord(root, "k") << "\n";
-  cout << FindWord(root, "pogba") <<"\n";
-  cout << FindWord(root, "manchester") <<"\n";
-  cout << FindWord(root, "manc") <<"\n";
+  root = addword(root, "avika");
+  root = addword(root, "Lingard");
 
-  Remove("k", root, 0, 1);
-  cout << FindWord(root, "k");
 
+  findword(root, "avika") == 1 ? cout <<"avika exists\n" : cout <<"avika does'nt exists\n";
+  findword(root, "avik") == 1 ? cout << "avik exists\n" : cout << "avik does'nt exists\n";
+  findword(root, "ava") == 1 ? cout << "ava exists\n" : cout <<"ava does'nt exists\n";
+  findword(root, "a") == 1 ? cout << "a exists\n" : cout <<"a does'nt exists\n";
+  findword(root, "Lingard") == 1 ? cout << "Lingard exists\n" : cout <<"Lingard does'nt exists\n";  
+
+  remove(&root, "ava", 0, 3); cout << "removal !!!!\n";
+  remove(&root, "avik", 0, 4); cout << "removal !!!!\n"; 
+
+  findword(root, "ava") == 1 ? cout << "ava exists\n" : cout <<"ava does'nt exists\n";
+  findword(root, "avik") == 1 ? cout << "avik exists\n" : cout <<"avik does'nt exists\n";
+  findword(root, "avika") == 1 ? cout << "avika exists\n" : cout <<"avika does'nt exists\n";
   return 0;
 
 }
